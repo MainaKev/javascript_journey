f87292b8a6dd69582e1cc231d3c36b6350bf452a
MainaKevv <mainakevv@gmail.com>
2022-06-01 21:45:17 +0300

    first javascript commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let js = 'amazing'
+if (js ==='amazing') alert ('javascript is fun')
+
+console.log(40 + 8 + 23 -10)
+
+console.log("Kevin Maina")
+console.log("25")
+
+let firstName = "Kevin"; 
+console.log(firstName);
+
+
+let javascriptIsFun = true;
+
+//data types
+console.log(typeof firstName);
+console.log(typeof 32);
+console.log(typeof js);
+console.log(typeof javascriptIsFun);
+
+//dynamic typing...changed from boolean to string
+javascriptIsFun = 'YES';
+console.log( typeof javascriptIsFun);
+
+
+
+
+
+
