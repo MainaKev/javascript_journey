0bf46e1c8a153f7f8e9068b06f27c0ec7fff3112
MainaKevv <mainakevv@gmail.com>
2022-06-03 00:18:58 +0300

    day 2 commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*DAY 1*/
+
+/*
+
 let js = 'amazing'
 if (js ==='amazing') alert ('javascript is fun')
 
@@ -21,6 +25,117 @@ console.log(typeof javascriptIsFun);
 //dynamic typing...changed from boolean to string
 javascriptIsFun = 'YES';
 console.log( typeof javascriptIsFun);
+
+
+let age = 30;
+
+age = 31;
+
+*/
+
+/*DAY 2 */
+
+let age = 30;
+age = 31;
+
+/*a let variable can be changed*/
+
+const birthYear = 1991;
+
+/*birthYear = 1990;
+const variable cannot be changed
+*/
+
+var job = 'programmer';
+job = 'teacher'
+
+/*a var variable can  also be changed*/
+
+
+lastName = 'Maina';
+console.log(lastName);
+
+
+/* MATH OPERATORS*/
+
+const now= 2037;
+const ageKevin = 2037 - 1994;
+const ageSarah = now - 2018;
+console.log(ageKevin, ageSarah);
+
+
+console.log(ageKevin * 2, ageSarah/2, 2**3);
+/* 2**3 means 2 to the power of 3 */
+
+const fName = 'Kev';
+const lName = 'Maina';
+console.log( fName + ' ' + lName); /*concatenating strings usi operators*/
+
+
+/*ASSIGNMENT OPERATORS*/
+let x = 10 + 5 ;
+
+x+= 10;
+x *= 2;
+x++;	/* x= x+1*/
+x--; 	/* x= x-1*/
+console.log(x);
+
+
+/*COMPARISON OPERATORS*/
+
+//console.log(ageKevin < ageSarah);
+
+/*  <,  >,  <=,  >=  */
+
+const isFullAge =ageSarah >= 18;
+
+console.log(now - 1994 > now -2018)
+
+let a,b;
+a=b= 25-10-5;
+console.log(a, b);
+
+const averageAge = (ageKevin + ageSarah) /2
+console.log(ageKevin, ageSarah, averageAge);
+
+
+
+
+let markHeight= 1.69, markMass =78;
+let johnHeight=1.95, johnMass=92
+
+let markBmi = markMass / (markHeight **2)
+console.log(markBmi) 
+
+let johnBmi = johnMass / (johnHeight **2)
+console.log(johnBmi)
+
+
+let markHigherBMI = markBmi > johnBmi
+
+console.log(markHigherBMI)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
